1a0132f85301c2548d8e716747e826ca20ba5688
victorborbasilva <131375634+victorborbasilva@users.noreply.github.com>
2023-08-09 21:21:46 -0300

    Add files via upload

---
+++ aula1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Página 1</title>
+    </head>
+<body>
+
+<h1>My first PHP page!</h1>
+
+<?php
+$color = "red";
+
+echo "My car is " . $color . "<br>";
+#variável usada corretamente
+
+echo "My house is " . $COLOR . "<br>";
+#variável usada incorretamente
+
+echo "My boat is " . $coLOR . "<br>";
+//variável usada corretamente
+
+$txt = "Hello World!";
+$x = 5;
+$y = 10.5;
+
+echo $x + $y;
+
+?> 
+
+
+
+</body>
+</html>
